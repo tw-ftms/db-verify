1930a1bff82a7612845f144dd97fbe4f2f8eac37
yandi527 <yandi.lin@thoughtworks.com>
2022-11-25 11:15:52 +0800

    [yandi] #N/A feat: get_database_connection method

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+import data_diff
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+def get_database_connection(database_property):
+    return data_diff.connect(database_property)
 
 
 if __name__ == '__main__':
